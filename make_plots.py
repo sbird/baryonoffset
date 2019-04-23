5a1df1bc12c8279990feeb613071a699add9702f
Simeon Bird <sbird@ucr.edu>
2019-04-23 14:42:29 -0700

    Commit plotting scripts

---
+++ make_plots.py
@@ -0,0 +1,133 @@
+"""Plot relative power spectra from simulations with Nbodykit"""
+import os
+import os.path
+import numpy as np
+import scipy.interpolate
+
+import matplotlib
+matplotlib.use("PDF")
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_pdf import FigureCanvasPdf
+
+simnames = {"L1000-baronlyglass": "BIGGLASS", "L300-adaptive": "ADAPTIVE", "L300-baryonlyglass-3": "HALFGLASS",
+            "L300-oversample": "UNDERSAMP", "L300" : "TWOGRID"
+           }
+
+lss = {"L1000-baronlyglass": "-", "L300-adaptive": "-.", "L300-baryonlyglass-3": "--",
+       "L300-oversample": "-", "L300" : "-"
+      }
+colors = {"L1000-baronlyglass": "blue", "L300-adaptive": '#1f77b4', "L300-baryonlyglass-3": '#d62728',
+          "L300-oversample": '#2ca02c', "L300" : '#bcbd22'
+         }
+colorsbar = {"L1000-baronlyglass": '#bcbdff', "L300-adaptive": "#7f7f7f", "L300-baryonlyglass-3": '#d627ff',
+             "L300-oversample": 'yellowgreen', "L300" : 'yellowgreen'
+            }
+datadir = os.path.expanduser("~/data/linear-growth-offset")
+
+plotdir = "plots"
+
+def modecount_rebin(kk, pk, modes, pkc, minmodes=20, ndesired=200):
+    """Rebins a power spectrum so that there are sufficient modes in each bin"""
+    assert np.all(kk) > 0
+    logkk=np.log10(kk)
+    mdlogk = (np.max(logkk) - np.min(logkk))/ndesired
+    istart=iend=1
+    count=0
+    pk_div = pk /pkc(kk)
+    k_list=[kk[0]]
+    pk_list=[pk_div[0]]
+    targetlogk=mdlogk+logkk[istart]
+    while iend < np.size(logkk)-1:
+        count+=modes[iend]
+        iend+=1
+        if count >= minmodes and logkk[iend-1] >= targetlogk:
+            pk1 = np.sum(modes[istart:iend]*pk_div[istart:iend])/count
+            kk1 = np.sum(modes[istart:iend]*kk[istart:iend])/count
+            k_list.append(kk1)
+            pk_list.append(pk1)
+            istart=iend
+            targetlogk=mdlogk+logkk[istart]
+            count=0
+    k_list = np.array(k_list)
+    pk_list = np.array(pk_list) * pkc(k_list)
+    return (k_list, pk_list)
+
+def get_saved_power(sdir, redshift, sp, pkc, kpc=True):
+    """Load a saved power spectrum from a file."""
+    scale = 1./(1+redshift)
+    fname = os.path.join(sdir, "output/%s-%.4f.txt" % (sp, scale))
+    pkk = np.loadtxt(fname)
+    ii = np.where(pkk[:,2] > 1)[0]
+    #Convert to Mpc/h units
+    if kpc:
+        pkk[ii, 0]*= 1e3
+        pkk[ii, 1]/=1e9
+    return modecount_rebin(pkk[ii, 0], pkk[ii, 1], pkk[ii, 2], pkc=pkc)
+
+def get_class_power(z, camb_transfer):
+    """Find the class baryon and DM power spectrum. Only works for snapshots at integer redshift."""
+    camb_trans = np.loadtxt(os.path.join(camb_transfer, "transfer.dat-"+str(int(np.round(z)))))
+    camb_mat = np.loadtxt(os.path.join(camb_transfer, "matterpow.dat-"+str(int(np.round(z)))))
+    intptot = scipy.interpolate.interp1d(camb_mat[:,0], camb_mat[:,1])
+    intpdm = scipy.interpolate.interp1d(camb_trans[:,0], camb_trans[:,3]/camb_trans[:,5])
+    intpbar = scipy.interpolate.interp1d(camb_trans[:,0], camb_trans[:,2]/camb_trans[:,5])
+    intpdmpk = scipy.interpolate.interp1d(camb_mat[:,0], intpdm(camb_mat[:,0])**2 * camb_mat[:,1])
+    intpbarpk = scipy.interpolate.interp1d(camb_mat[:,0], intpbar(camb_mat[:,0])**2 * camb_mat[:,1])
+    return camb_mat[:,0], intpbarpk, intpdmpk, intptot
+
+def plot_power(zz, sims, plottitle, total=False):
+    """Check the initial power against linear theory and a linearly grown IC power"""
+
+    #Check types have the same power
+    fig = Figure()
+    canvas = FigureCanvasPdf(fig)
+    ax = fig.add_subplot(111)
+
+    xmin = 0.1
+    ymin = 0.92
+    fig2 = Figure()
+    canvas2 = FigureCanvasPdf(fig2)
+    ax2 = fig2.add_subplot(111)
+    for ss in sims:
+        sdir = os.path.join(datadir, ss)
+        if ss == sims[0]:
+            classkk, classbarpk, classdmpk, classtot = get_class_power(zz, os.path.join(datadir, ss))
+            ax.plot(classkk, classbarpk(classkk)/ classdmpk(classkk), ls=":", label='CLASS', color="grey")
+        kkcdm, pkcdm, = get_saved_power(sdir, zz, "power-DM", classdmpk)
+        kkbar, pkbar, = get_saved_power(sdir, zz, "power-bar", classbarpk)
+        #Note k in kpc/h
+        ax2.axhline(1, ls=":", color="grey")
+        ax.plot(kkcdm, pkbar / pkcdm, ls=lss[ss], color=colors[ss], label=simnames[ss])
+        ii = np.where(kkcdm < 0.2)
+        ymin = np.min([np.min(pkbar[ii]/pkcdm[ii])-0.02, ymin])
+        ax2.plot(kkbar, pkbar/classbarpk(kkbar) , ls=lss[ss], color=colorsbar[ss], label=simnames[ss]+" baryon")
+        ax2.plot(kkcdm, pkcdm/classdmpk(kkcdm) , ls=lss[ss], color=colors[ss], label=simnames[ss]+" DM")
+        xmin = np.min([xmin, kkcdm[0]])
+        if total:
+            kktot, pktot = get_saved_power(sdir, zz, "powerspectrum", classtot, kpc=False)
+            ax2.plot(kktot, pktot/classtot(kktot), label=simnames[ss]+" Total", ls=lss[ss], color="black")
+
+    ax.set_xlabel("k (h/Mpc)")
+    ax.set_ylabel(r"$P_\mathrm{b} / P_\mathrm{CDM} (k, z=%d)$" % zz )
+    ax.set_xscale('log')
+    ax.set_xlim(xmin/2., 2)
+    ax.set_ylim(ymin, 1.04)
+    ax.legend(loc="upper left")
+    fig.tight_layout()
+    fig.savefig(os.path.join(plotdir, plottitle + '_%d_relpower.pdf' % zz))
+    fig.clf()
+    ax2.set_xlabel("k (h/Mpc)")
+    ax2.set_ylabel(r"$P_\mathrm{SIM} / P_\mathrm{CLASS} (k, z=%d)$" % zz )
+    ax2.set_xscale('log')
+    ax2.set_xlim(xmin/2., 2)
+    ax2.set_ylim(0.9, 1.15)
+    ax2.legend(loc="upper left")
+    fig2.tight_layout()
+    fig2.savefig(os.path.join(plotdir, plottitle + '_%d_class.pdf' % zz))
+    fig2.clf()
+
+if __name__ == "__main__":
+    for red in (2, 4, 9):
+        plot_power(red, ["L300"], "literature", total=True)
+        plot_power(red, ["L300-baryonlyglass-3", "L1000-baronlyglass"], "halfglass")
+        plot_power(red, ["L300-baryonlyglass-3", "L300-oversample","L300-adaptive"], "oversample")
---
+++ make_species_power.py
@@ -0,0 +1,51 @@
+"""Make the per-species power spectrum using NBodyKit"""
+import os.path as path
+import sys
+import glob
+import numpy
+from nbodykit.lab import BigFileCatalog,FFTPower
+import timeit
+
+def sptostr(sp):
+    """Get a string from a species"""
+    if sp == 0:
+        return "bar"
+    elif sp == 1:
+        return "DM"
+    elif sp == 2:
+        return "nu"
+    return ""
+
+def compute_power(output, Nmesh=1024, species=1, spec2 = None):
+    """Compute the compensated power spectrum from a catalogue."""
+    catnu = BigFileCatalog(output, dataset=str(species)+'/', header='Header')
+    sp = sptostr(species)
+    sp2 = sptostr(spec2)
+    outfile = path.join(output,"../power-"+sp+sp2+"-%.4f.txt" % catnu.attrs["Time"][0])
+    if path.isfile(outfile):
+        return
+    catnu.to_mesh(Nmesh=Nmesh, window='cic', compensated=True, interlaced=True)
+    if spec2 is not None:
+        catcdm = BigFileCatalog(output, dataset=str(spec2)+'/', header='Header')
+        catcdm.to_mesh(Nmesh=Nmesh, window='cic', compensated=True, interlaced=True)
+        pkcross = FFTPower(catnu, mode='1d', Nmesh=1024,second = catcdm, dk=5.0e-6)
+        power = pkcross.power
+    else:
+        pknu = FFTPower(catnu, mode='1d', Nmesh=1024, dk=5.0e-6)
+        power = pknu.power
+    numpy.savetxt(outfile,numpy.array([power['k'], power['power'].real,power['modes']]).T)
+    return power
+
+def all_compute(directory):
+    """Do computation for all snapshots in a directory"""
+    snaps = glob.glob(path.join(directory, "output/PART_[0-9][0-9][0-9]"))
+    for ss in snaps:
+        try:
+            compute_power(ss)
+            compute_power(ss,species=0)
+        except IOError:
+            print("Could not read snapshot", ss)
+            pass
+
+if __name__ == "__main__":
+    all_compute(sys.argv[1])
